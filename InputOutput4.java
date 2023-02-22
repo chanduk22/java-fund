5f8dfcbd4f1c0f2d9151e2423033d280405b9172
K.CHANDU KUMAR <126059033+chanduk22@users.noreply.github.com>
2023-02-22 20:21:56 +0530

    InputOutput4.java

---
+++ InputOutput4.java
@@ -0,0 +1,22 @@
+/* Write a program to get the string as input and print it.
+
+Sample Input :
+
+Programmer
+
+Sample Output :
+
+Programmer */
+
+
+import java.util.Scanner;
+public class InputOutput4
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(s);
+        //write your code
+    }
+}
