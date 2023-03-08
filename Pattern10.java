1e90a1ff5852ed62f809ff4b3306547e106764ea
K.CHANDU KUMAR <126059033+chanduk22@users.noreply.github.com>
2023-03-08 22:14:05 +0530

    Pattern10.java

---
+++ Pattern10.java
@@ -0,0 +1,69 @@
+/* Write a Python program to print the given pattern from user-defined values(dynamic values).
+
+Sample input 1:
+5
+Sample output 1:
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    * 
+
+Sample input 2:
+8
+Sample output 2:
+       *
+      ***
+     *****
+    *******
+   *********
+  ***********
+ *************
+***************
+ *************
+  ***********
+   *********
+    *******
+     *****
+      ***
+       *              */
+
+
+import java.util.Scanner;
+public class Pattern10
+{
+    public static void main(String[] args)
+    {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       for(int i=1;i<=n;i++)
+       {
+           for( int s=n;s>i;s--)
+           {
+               System.out.print(" ");
+           }
+           for(int j=1;j<=(i*2)-1;j++)
+           {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+       for(int i=n-1;i>=1;i--)
+       {
+           for( int s=1;s<=n-i;s++)
+           {
+               System.out.print(" ");
+           }
+           for(int j=1;j<=(i*2)-1;j++)
+           {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+       
+    }
+}
